1fa4e48a08b19188fa9b6273263ff05b6a979e69
Dave Lavilla <davelavilla@gmail.com>
2022-06-27 20:42:44 +0800

    created starting pseudo code for log-in page

---
+++ src/login-form.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+
+function LogInPage() {
+    const [name, setName] = useState("")
+    const [password, setPassword] = useState("")
+
+    const changeName = (e) => {
+        setName(e.target.value)
+    }
+    const changePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+    return (
+        <div>
+            <div>
+                <input type="text" onChange={() => setName(changeName)} />
+                <input type="text" onChange={() => setPassword(changePassword)} />
+            </div>
+
+            <div>
+                <p>{name}</p>
+                <p>{password}</p>
+            </div>
+        </div>
+    )
+}
+
+export default LogInPage
