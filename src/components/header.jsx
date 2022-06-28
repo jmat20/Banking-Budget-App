98c0fc0be1101bab950e3f84e85618798f113372
Dave Lavilla <davelavilla@gmail.com>
2022-06-28 17:33:44 +0800

    cleaned folders

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './header.css';
+import logo from '../assets/images/logo.svg';
+import "../assets/scss/header.css";
 
 let HeaderComponent = () => {
     return (
