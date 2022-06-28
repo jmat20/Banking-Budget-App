3a98cc82bb97c36b6bd9f8edc94e98af61915b5b
Dave Lavilla <davelavilla@gmail.com>
2022-06-28 17:13:06 +0800

    fixed errors in app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 import React from "react";
 import LogInPage from "./components/login-form";
-=======
-import logo from "./logo.svg";
-import "./App.css";
->>>>>>> df102c44bd8dd0219c72503acf581f6491954e09
 
 function App() {
-  return <div className="App"></div>;
+  return <LogInPage />;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
 import App from "./App";
-=======
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "./header.css";
-import LogInPage from "./login-form";
-import HeaderComponent from "./header";
->>>>>>> df102c44bd8dd0219c72503acf581f6491954e09
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <HeaderComponent />
-    <LogInPage />
+    <App />
   </React.StrictMode>
 );
