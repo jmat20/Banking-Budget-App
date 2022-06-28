37c9f161a23d6be98d24423ce522389eaa85f668
Dave Lavilla <davelavilla@gmail.com>
2022-06-28 18:04:46 +0800

    set const for admin and deconstructed array for users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import HeaderComponent from "./components/header";
+import LogIn from "./components/login-form";
 
 function App() {
+  const adminUser = {
+    username: "admin",
+    password: "placeholderpass",
+  };
+
+  const [user, setUser] = useState({ name: "", email: "" });
+
   return <HeaderComponent />;
 }
 
