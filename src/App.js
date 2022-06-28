df102c44bd8dd0219c72503acf581f6491954e09
John Ezekiel Mateo <johnezekielmateo@gmail.com>
2022-06-28 17:05:53 +0800

    adds header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
-import LogInPage from "./login-form";
 
 function App() {
-  return (
-    <div className="App">
-      <LogInPage />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
